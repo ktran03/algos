42c176441f6531673ea9e14d2aef00626148d3c6
ktran03 <khanh.tran.vinh@gmail.com>
2014-05-06 23:33:25 -0400

    quick sort

---
+++ algos/KTQuickSort.h
@@ -0,0 +1,13 @@
+//
+//  KTQuickSort.h
+//  algos
+//
+//  Created by khanh tran on 2014-05-06.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KTQuickSort : NSObject
+
+@end
---
+++ algos/KTQuickSort.m
@@ -0,0 +1,13 @@
+//
+//  KTQuickSort.m
+//  algos
+//
+//  Created by khanh tran on 2014-05-06.
+//  Copyright (c) 2014 ktran03. All rights reserved.
+//
+
+#import "KTQuickSort.h"
+
+@implementation KTQuickSort
+
+@end
